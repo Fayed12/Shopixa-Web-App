fa01bcb5b91417ae7d42f27c1266d3a48cb02158
mohamed fayed <memad4858@gmail.com>
2025-10-25 22:44:55 +0300

    add products and data.json files, edit API Links, and some files

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+
+const BASE_URL = "http://localhost:5000";
+
+export const API_ROUTES = {
+    PRODUCTS: `${BASE_URL}/products`,
+    USERS: `${BASE_URL}/users`,
+    ADMIN: `${BASE_URL}/admin`,
+}
